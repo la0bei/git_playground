d8defccb9bec81bc1196cd8bf606930b80b0a9c3
stackhacks <stackhacks828@gmail.com>
2016-03-27 15:29:53 +0800

    modify from develop2 branch

--- helloworld.c
+++ helloworld.c
@@ -6,5 +6,6 @@ int main()
 	cout << "hello, world" << endl;
 	cout << "welcome to github" << endl;
 	cout << "welcome to git world" << endl;
+	cout << "modify from develop2 branch" << endl;
     return 0;
 }
